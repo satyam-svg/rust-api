c3385826390b90e084e9f988ad9844876a97d996
Praveen <satyammaurya9620@gmail.com>
2024-01-05 13:42:19 +0530

    made 4 end points

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
-#[macro_use] extern crate rocket;
-// Try visiting:
-//   http://127.0.0.1:8000/hello/world
+#[macro_use]
+extern crate rocket;
+
+
+
 #[get("/world")]
 fn world() -> &'static str {
     "Hello, world!"
 }
+
+#[post("/users")]
+fn get_create_one() -> &'static str {
+    "user created"
+}
+#[put("/users")]
+fn get_put_one() -> &'static str {
+    "user put created"
+}
+#[delete("/users")]
+fn get_delete_one() -> &'static str {
+    "user delete successfully"
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/hello", routes![world])
-        
+        .mount("/", routes![get_create_one,get_put_one,get_delete_one])
 }
